65d562465719633cfd7a41c0d4241c49759e8c7c
Benjamin Marsh <71513348+BenMarshLS@users.noreply.github.com>
2021-11-18 03:04:55 +0000

    added memorial page added contact page added slideshow element

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ def index():
 def contact():
   return render_template("contact.html")
 
+@app.route("/charity", methods= ["GET", "POST"])
+def charity():
+  return render_template("charity.html")
+
+@app.route("/memory", methods= ["GET", "POST"])
+def memory():
+  return render_template("memory.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=random.randint(2000, 9000))
